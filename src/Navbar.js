6b9e3cdbd0d09c6008c7279d7aa10071760595b3
Theodore Truong <tqtruo@gmail.com>
2019-12-18 13:37:08 -0600

    Navbar| search function working

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
-const Navbar = (props) =>{
-
-    return (
-        <div>
-            <form className="search-form" onSubmit={props.getQuery}>
-				<input className="search-box" type="text" placeholder="Search for a recipe!" onChange={props.getSearch} />
-				<button className="search-button" type="submit">
-					Search
-				</button>
-			</form>
-        </div>
-    )
-}
+const Navbar = (props) => {
+	return (
+		<ul>
+			<li className="home">Home</li>
+			<li className="about">About</li>
+			<li className="searchform">
+				<form className="search-form" onSubmit={props.getQuery}>
+					<input
+						className="search-box"
+						type="text"
+						placeholder="Search for a recipe!"
+						onChange={props.getSearch}
+					/>
+					<button className="search-button" type="submit">
+						Search
+						{/* <i className="material-icons">search</i> */}
+					</button>
+				</form>
+			</li>
+		</ul>
+	);
+};
 
 export default Navbar;
